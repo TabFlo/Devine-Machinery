bb5323ef006efca00caa8e8929c1903d07a3df2c
TabFlo <s2210238024@fhooe.at>
2024-12-12 14:39:29 +0100

    short script

---
+++ Assets/OnStartScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using PixelCrushers.DialogueSystem;
+using PixelCrushers.DialogueSystem.Wrappers;
+using UnityEngine;
+
+public class OnStartScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    DialogueManager.StartConversation("start");   
+    }
+
+}
