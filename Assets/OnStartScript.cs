9a4bf35e41b2a038b73c9b0528cb3c6531e3fd6b
TabFlo <s2210238024@fhooe.at>
2025-01-19 02:42:13 +0100

    second dialogue almost done (+3 and -3 appro dialogue option 3 not added yet)

--- Assets/OnStartScript.cs
+++ Assets/OnStartScript.cs
@@ -9,7 +9,7 @@ public class OnStartScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-    DialogueManager.StartConversation("testRND");   
+    DialogueManager.StartConversation("MainDialogue1");   
     }
 
     public void startDialogue()
