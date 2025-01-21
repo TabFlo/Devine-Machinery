b14c6565f46e2cf0635860d9f212350f8fac2508
feldbacherSimone <simone.feldbacher@gmail.com>
2025-01-21 12:21:59 +0100

    WIP/SerialManager API for reading Sensor data

--- Assets/SerialManager.cs
+++ Assets/SerialManager.cs
@@ -3,22 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
-
+using JetBrains.Annotations;
 using TMPro;
+
+public enum DATA_TYPE {
+    COLOR, 
+    FLOAT, 
+    STATE,
+}
+
+public enum BODY_PART {
+    EYE, 
+    TORSO, 
+    CHEST, 
+    HAND_L, 
+    HAND_R, 
+}
 
 public class SerialManager : MonoBehaviour
 {
+    // Setup
     SerialPort stream = new SerialPort("COM15", 9600);
-    [SerializeField] private int buttonState = 0;
-    [SerializeField] private string value; 
-    [SerializeField] private Color color; 
-    private TextMeshProUGUI text;
+    [SerializeField] private float readRate;
     
-    // Start is called before the first frame update
+    // Testing
+    [SerializeField] private Color color;
+
+    
+    private float timeSinceLastRead;
+    private String currentReadLine;
+
+
+    private Dictionary<BODY_PART, float> sensorData = new Dictionary<BODY_PART, float>();
+
+    // Unity Functions
     void Start()
     {
         if (!stream.IsOpen){
-            
             try
             {
                 stream.Open();
@@ -28,21 +49,47 @@ public class SerialManager : MonoBehaviour
                 Debug.Log("Failed tp Open Serial port:" + e.Message);
             }
         }
-
-    text = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        ReadData(stream);
-        int.TryParse(value, out buttonState);
-        text.text = buttonState.ToString();
+        ReadToFData();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SendData(color.ToString());
+        }
+    }
+    
+    // API Methods
+    
+    /// <summary>
+    /// Returns sensor Data from various body Parts
+    /// </summary>
+    /// <param name="bodyPart"></param>
+    /// <returns>Value is always a float, needs to be parsed to other data types if needed</returns>
+    public float GetSensorData(BODY_PART bodyPart)
+    {
+        return sensorData[bodyPart];
+    }
+    
+    
+    // Private Methods 
+
+    public void ReadToFData()
+    {
+        BODY_PART[] parts = { BODY_PART.HAND_L, BODY_PART.HAND_R };
+        var output = -1;
+        
+        foreach (BODY_PART bodyPart in parts)
+        {
+            if (TryReadData(stream, ref currentReadLine) && currentReadLine.Contains(bodyPart.ToString()))
+            {
+                currentReadLine = currentReadLine.Replace(bodyPart.ToString(), "");
+                TryParseData(DATA_TYPE.FLOAT, ' ', currentReadLine, ref output);
+            }
+            sensorData[bodyPart] = output; 
         }
     }
 
@@ -55,16 +102,85 @@ public class SerialManager : MonoBehaviour
         }
     }
 
-    void ReadData(SerialPort stream)
+    
+    void sendColorData(Color c, [CanBeNull] String prefix)
     {
-        if (stream.IsOpen && stream.BytesToRead > 0)
+        int r = Mathf.RoundToInt(c.r * 255); 
+        int g = Mathf.RoundToInt(c.g * 255); 
+        int b = Mathf.RoundToInt(c.b * 255);
+        
+        String msg = String.Join(r.ToString(), g.ToString(), b.ToString());
+        
+        if (!String.IsNullOrEmpty(prefix))
         {
-
-            value = stream.ReadLine();
-            Debug.Log(value);
+            msg = String.Join(prefix, msg);
         }
+        SendData(msg);
     }
 
+
+    bool TryReadData(SerialPort stream, ref String currentReadLine)
+    {
+        if (timeSinceLastRead >= 1/readRate && stream.IsOpen && stream.BytesToRead > 0)
+        {
+            String value = stream.ReadLine();
+            Debug.Log(value);
+            currentReadLine = value; 
+            timeSinceLastRead = 0;
+            return true; 
+        }
+
+        timeSinceLastRead += Time.deltaTime;
+        return false; 
+    }
+
+    //TODO: Refactor this bad boi 
+    private bool TryParseData<T>(DATA_TYPE dataType, char delimiter, string data, ref T output)
+    {
+        String[] slices = data.Split(delimiter);
+        String prefix = slices[0];
+        
+        
+        if (!String.Equals(prefix, dataType.ToString())) return false; 
+        
+        switch (dataType)
+        {
+            case DATA_TYPE.FLOAT:
+                if (float.TryParse(slices[1], out float resultF))
+                {
+                    output = (T)Convert.ChangeType(resultF, typeof(T));
+                    return true; 
+                }
+                break; 
+            
+            case DATA_TYPE.COLOR:
+                if (slices.Length != 4) break;
+                
+                float[] col = new float[3];
+                for (int i = 1; i < slices.Length; i++)
+                {
+                    if (!float.TryParse(slices[i], out col[i - 1])) break;
+                    col[i - 1] /= 255;
+                }
+
+                Color resultC = new Color(col[0], col[1], col[2]); //TODO: add additional checks on the color Value
+                output = (T)Convert.ChangeType(resultC, typeof(T));
+                return true;
+
+            case DATA_TYPE.STATE: 
+                if (bool.TryParse(slices[1], out bool resultB))
+                {
+                    output = (T)Convert.ChangeType(resultB, typeof(T));
+                    return true;
+                }
+                break;
+        }
+
+        Debug.LogWarning($"Could not parse Data: {data} to data Type {dataType}");
+        return false; 
+    }
+
+    // Housekeeping
     private void OnApplicationQuit()
     {
         if (stream != null && stream.IsOpen)
